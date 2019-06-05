49e43a090c01009d4b2e0f4fd17b160df17e082e
casperklein <casperklein@users.noreply.github.com>
2019-06-06 00:23:16 +0200

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# https://askubuntu.com/questions/165027/why-cant-my-program-find-the-libraries-in-usr-local-lib
+
 exe dpkg -i libevent-2.0.22_2.0.22-1_amd64.deb tmux_2.0-1_amd64.deb
+echo
+exe aptitude install libevent-2.0-5
 echo
 
 exe echo '>>' /etc/apt/preferences
