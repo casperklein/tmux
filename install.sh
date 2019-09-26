38b7b63ab340bf479ebe2ff3b7d11a1e26e43e26
Casper <casperklein@users.noreply.github.com>
2019-09-26 17:53:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 # https://askubuntu.com/questions/165027/why-cant-my-program-find-the-libraries-in-usr-local-lib
 
-exe dpkg -i libevent-2.0.22_2.0.22-1_amd64.deb tmux_2.0-1_amd64.deb
+exe apt-get update '&&' apt-get -y install libncurses5
 echo
-exe aptitude install libevent-2.0-5 libncurses5
+exe dpkg -i libevent-2.0.22_2.0.22-1_amd64.deb tmux_2.0-1_amd64.deb
 echo
 
 exe echo '>>' /etc/apt/preferences
